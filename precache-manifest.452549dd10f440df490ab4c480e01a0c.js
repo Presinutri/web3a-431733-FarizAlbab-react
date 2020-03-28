dca5d585f9e13a9d453add046bfb834138f872e4
farizalbab <farizalbab@gmail.com>
2020-03-28 15:46:45 +0700

    Updates

---
+++ precache-manifest.452549dd10f440df490ab4c480e01a0c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c4c8a33d3c0e53217e39d0ef33b1b74",
+    "url": "/presinutri.github.io/adadas/index.html"
+  },
+  {
+    "revision": "5331089db1dac252345b",
+    "url": "/presinutri.github.io/adadas/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "67f04e5a360de8f77b63",
+    "url": "/presinutri.github.io/adadas/static/css/main.38a5a20d.chunk.css"
+  },
+  {
+    "revision": "5331089db1dac252345b",
+    "url": "/presinutri.github.io/adadas/static/js/2.fcfe33d7.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/presinutri.github.io/adadas/static/js/2.fcfe33d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67f04e5a360de8f77b63",
+    "url": "/presinutri.github.io/adadas/static/js/main.c3e2370b.chunk.js"
+  },
+  {
+    "revision": "61ded9cdd5f1d76c4a33",
+    "url": "/presinutri.github.io/adadas/static/js/runtime-main.de898337.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/presinutri.github.io/adadas/static/media/logo.5d5d9eef.svg"
+  }
+]);
