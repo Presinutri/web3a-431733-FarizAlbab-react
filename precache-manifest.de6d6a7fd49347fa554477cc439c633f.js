dce08c1d25552657f5bbfee837aacbd55316cfa6
farizalbab <farizalbab@gmail.com>
2020-03-28 15:58:28 +0700

    Updates

---
+++ precache-manifest.de6d6a7fd49347fa554477cc439c633f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b1e7eea663423f6fec8393d84d9fb39",
+    "url": "/web3a-431733-farizalbab-react/index.html"
+  },
+  {
+    "revision": "5331089db1dac252345b",
+    "url": "/web3a-431733-farizalbab-react/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "0213e2f2ad37162ef2f2",
+    "url": "/web3a-431733-farizalbab-react/static/css/main.38a5a20d.chunk.css"
+  },
+  {
+    "revision": "5331089db1dac252345b",
+    "url": "/web3a-431733-farizalbab-react/static/js/2.fcfe33d7.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/web3a-431733-farizalbab-react/static/js/2.fcfe33d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0213e2f2ad37162ef2f2",
+    "url": "/web3a-431733-farizalbab-react/static/js/main.e6373b99.chunk.js"
+  },
+  {
+    "revision": "4c80a799985ae427907a",
+    "url": "/web3a-431733-farizalbab-react/static/js/runtime-main.5c8bcb3c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web3a-431733-farizalbab-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/presinutri.github.io/web3a-431733-farizalbab-react/precache-manifest.8f5623d36e561720ffec74879194e3f8.js"
+  "/web3a-431733-farizalbab-react/precache-manifest.de6d6a7fd49347fa554477cc439c633f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/presinutri.github.io/web3a-431733-farizalbab-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/web3a-431733-farizalbab-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
